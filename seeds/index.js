e951542a2b702c65a7a319b14f4b196f50989f3f
Sujal Vaidya <sujalyatin@gmail.com>
2024-07-26 22:14:27 +0530

    Added basic styles and layout

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await HikingTrail.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random493 = Math.floor(Math.random() * 493);
+        const price = Math.floor(Math.random() * 300) + 20;
         const trail = new HikingTrail({
             location: `${cities[random493].city}, ${cities[random493].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur beatae doloremque ea, eius facilis fugit illo, labore molestias, nam nihil nisi quasi quo rerum sed veniam. Aperiam distinctio sed voluptate?',
+            price: price,
         })
         await trail.save();
     }

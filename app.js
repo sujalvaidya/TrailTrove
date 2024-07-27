5123957efe3b6fe369c29197943077d3da247a1c
Sujal Vaidya <sujalyatin@gmail.com>
2024-07-27 15:02:57 +0530

    Added error handling and data validation

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const {hikingTrailSchema} = require('./schemas');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const HikingTrail = require('./models/hikingTrail');
 
@@ -22,6 +26,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride('_method'))
 
+const validateHikingTrail = (req, res, next) => {
+
+    const {error} = hikingTrailSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(ele => ele.message).join(', ');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('home');
 });
@@ -35,34 +50,46 @@ app.get('/hikingTrails/new', (req, res) => {
     res.render('hikingTrails/new')
 })
 
-app.post('/hikingTrails', async (req, res) => {
+app.post('/hikingTrails', validateHikingTrail, catchAsync(async (req, res, next) => {
+    // if (!req.body.hikingTrail) throw new ExpressError('Invalid Hiking Trail Data', 400);
+
     const hikingTrail = new HikingTrail(req.body.hikingTrail);
     await hikingTrail.save();
     res.redirect(`/hikingTrails/${hikingTrail._id}`);
-})
+}));
 
-app.get('/hikingTrails/:id', async (req, res) => {
+app.get('/hikingTrails/:id', catchAsync(async (req, res) => {
     const hikingTrail = await HikingTrail.findById(req.params.id);
     res.render('hikingTrails/show', {hikingTrail});
-});
+}));
 
-app.get('/hikingTrails/:id/edit', async (req, res) => {
+app.get('/hikingTrails/:id/edit', catchAsync(async (req, res) => {
     const hikingTrail = await HikingTrail.findById(req.params.id);
     res.render('hikingTrails/edit', {hikingTrail});
 
-})
+}));
 
-app.put('/hikingTrails/:id', async (req, res) => {
+app.put('/hikingTrails/:id', validateHikingTrail, catchAsync(async (req, res) => {
     const {id} = req.params;
     const hikingTrail = await HikingTrail.findByIdAndUpdate(id, {...req.body.hikingTrail});
     res.redirect(`/hikingTrails/${hikingTrail._id}`);
-})
+}));
 
-app.delete('/hikingTrails/:id', async (req, res) => {
+app.delete('/hikingTrails/:id', catchAsync(async (req, res) => {
     const {id} = req.params;
     await HikingTrail.findByIdAndDelete(id);
     res.redirect('/hikingTrails');
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
 })
+
+app.use((err, req, res, next) => {
+    const {statusCode = 500} = err;
+    if (!err.message) err.message = 'Something went wrong';
+    res.status(statusCode).render('error', {err});
+});
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
